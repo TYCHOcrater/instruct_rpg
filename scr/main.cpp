c3ea877d8833e49fa02dd9fb582b45543d58f7f8
TYCHOcrater <paketpaper@gmail.com>
2019-02-28 02:06:29 +0200

    done

--- scr/main.cpp
+++ scr/main.cpp
@@ -25,3 +25,160 @@
 
 #define BOLD(x) "\x1B[1m" x RST
 #define UNDL(x) "\x1B[4m" x RST
+
+using namespace std;
+
+int main (void) {
+    strncat(time(NULL));
+
+    MyTeam squad;
+    Market ThisMarket(&squad);
+    Grid Gameplay(&ThisMarket);
+
+    ifstream Potions("input/Potions.txt");
+    if(!Potions){
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Armory("input/Armory.txt");
+    if(!Armory){
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Weaponry("input/Weaponry.txt");
+    if(!Weaponry) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+
+    ifstream FireSpells("input/FireSpells.txt");
+    if(!FireSpells) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream IceSpells("input/IceSpells.txt");
+    if(!IceSpells) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream LightingSpells("input/LightingSpells.txt");
+    if(!LightingSpells) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Warriors("input/Warriors.txt");
+    if(!Warriors) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Sorcerers("input/Sorcerers.txt");
+    if(!Sorcerers) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Paladins("input/Paladins.txt");
+    if(!Paladins) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+
+    ifstream Dragons("input/Dragons.txt");
+    if(!Dragons){
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Spirits("input/Spirits.txt");
+    if(!Spirits) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    ifstream Exoskeletons("input/Exoskeletons.txt");
+    if(!Exoskeletons) {
+        cout << "Cannot open file\n" << endl;
+        return 1;
+    }
+    string name;
+    int cost, level, RI;
+    while(Potions >> name >> cost >> level >> RI) {
+        ThisMarket.Insert_Potion(name,cost,level,RI);
+    }
+
+    int damage, handControl;
+    while(Weaponry >> name >> cost >> level >> damage >> handControl) {
+        ThisMarket.Insert_Weapon(name,cost,level,damage,handControl);
+    }
+
+    while(Armory >> name >> cost >> level >> RI) {
+        ThisMarket.Insert_Armor(name,cost,level,RI);
+    }
+
+    string nam;
+    int mp,strength,agility,dexterity,money,exp;
+    while(Warriors >> nam >> mp >> strength >> agility >> dexterity >> money >> exp) {
+        squad.Insert_Warrior(nam, mp, strength, agility, dexterity, money, exp);
+    }
+    while(Sorcerers >> nam >> mp >> strength >> agility >> dexterity >> money >> exp) {
+        squad.Insert_Sorcerers(nam, mp, strength, agility, dexterity, money, exp);
+    }
+    while(Paladins >> nam >> mp >> strength >> agility >> dexterity >> money >> exp) {
+        squad.Insert_Paladins(nam, mp, strength, agility, dexterity, money, exp);
+    }
+
+    int minL, loss, mP, red;
+    while(IceSpells >> nam >> cost >> minL >> loss >> mP >> red) {
+        ThisMarket.Insert_IceSpell(nam, cost, minL, loss, mP, red);
+    }
+    while(FireSpells >> nam >> cost >> minL >> loss >> mP >> red) {
+        ThisMarket.Insert_FireSpell(nam, cost, minL, loss, mP, red);
+    }
+    while(LightingSpells >> nam >> cost >> minL >> loss >> mP >> red) {
+        ThisMarket.Insert_LightningSpell(nam, cost, minL, loss, mP, red);
+    }
+
+    int dam, def, att, dam_r;
+    while( Dragons >> nam >> level >> dam >> def >> att >> dam_r) {
+        GamePlay.Insert_Dragon( nam, level, dam, def, att, dam_r);
+    }
+
+    while( Spirits >> nam >> level >> dam >> def >> att >> dam_r) {
+        GamePlay.Insert_Spirit( nam, level, dam, def, att, dam_r);        
+    }
+    while( Exoskeletons >> nam >> level >> dam >> def >> att >> dam_r) {
+        GamePlay.Insert_Exoskeleton( nam, level, dam, def, att, dam_r);        
+    }
+
+    cout << endl << endl;
+    cout << '\t' << KGRN << "\t\t\t\tK I N G D O M    O F   F R Y V I E L:   R E B I R T H" << KWHT << endl;
+    cout << '\t' << KCYN << "--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--" << KWHT << endl << endl;
+    cout << '\t' << "\t" << "In the ancient times king Arowen , the conquerer of the Sun Empire,after defeating the commander" << endl <<  '\t' << '\t' << "of the evil troops Manticore, bestowed peace amongst humans and builted his capitol" << endl;
+    cout << '\t' << "\t" << "named after his only daughter and sole succesor to the throne Fryviel. The princess , though as   " << endl << '\t' << '\t' << "she was born with the gift of prophecy foretold a grim future for the human nation " << endl;
+    cout << '\t' << "\t" << "for the Queen of Evil, the Mother of Monsters, the vicious Echidna was still not dead and hiding" << endl << '\t' << '\t' << " somewhere inside the kingdom was slowly rising a new army to detroy them all..." << endl;
+    cout << '\t' << "\t" << "But the night before her father's death, Fryviel saw a bright spot to their kingdom's future." << endl << '\t' << '\t' << "She spoke of a final victory someone would bring,wether a single man or a team of heroes " << endl;
+    cout << '\t' << "\t" << "would fight the enemies and restore peace to their great kingdom. And finally that day came..." << endl << '\t' << '\t' << " " << endl;
+    cout << endl;
+
+    cout << KYEL << "       .--.   _," << KWHT << endl;
+    cout << KYEL << "        .--;    7 /(_" << KWHT << endl;
+    cout << KYEL << "       /    '.   |   '-._    . ' ." << KWHT << endl;
+    cout << KYEL << "      |       7  7    ,-.)  -= * =-" << KWHT << endl;
+    cout << KYEL << "       7 /7_   '. 7((` .(    '/. '" << KWHT   << endl;
+    cout << KYEL << "        )! /     7 )7  _/   _/" << KWHT << "\t\t   A new adventure will shortly begin!" << endl;
+    cout << KYEL << "       /  7!    .-'   '--. /_7" << KWHT << endl;
+    cout << KYEL << "      |    7!_.' ,        7/||" <<  KWHT  << endl;
+    cout << KYEL << "      7     _.-';,_) _)'7 7||" << KWHT << "\t\tNow it's up to you to decide who the hero or heroes will be." << endl;
+    cout << KYEL << "       '.       /`9   (   '._/" << KWHT << endl;
+    cout << KYEL << "         `7   .;  |  . '." << KWHT << "\t\t  Get ready to roam the kingdom and fight for its glory." << endl;
+    cout << KYEL << "           ).'  )/|       ?" << KWHT << endl;
+    cout << KYEL << "           `    ` |  6|   |" << KWHT << "\t\tFor the time has arrived that the war will spread"<< endl;
+    cout << KYEL << "                   7  |   |" << KWHT << "\t\tlike hellfire so that peace can be bestowed once more." << endl;
+    cout << KYEL << "                    '.|   |" << KWHT << endl;
+    cout << KYEL << "                       !  '7__" << KWHT << "\t\tWith the gift of wisdom these Fae have given you and" << endl;
+    cout << KYEL << "                        `-._  '. _" << KWHT << "\t\tthe courage you have start your journey and may" << endl;
+    cout << KYEL << "                           7`;-.` `._" << KWHT << "\t\tit be victorious!" << endl;
+    cout << KYEL << "                            7 ! `'-._7" << KWHT << endl;
+    cout << KYEL << "                             7 |" << KWHT << endl;
+    cout << KYEL << "                              7 )" << KWHT << endl;
+    cout << KYEL << "                               7_7" << KWHT << endl;
+
+    Gameplay.gameplay();
+}
--- scr/myTeam.cpp
+++ scr/myTeam.cpp
@@ -28,6 +28,170 @@
 
 using namespace std;
 
-MyTeam::MyTeam() {
-    //insert all heroes from files
+MyTeam::MyTeam() {}
+
+void MyTeam::Insert_Warrior(string name, int magicP, int strength, int agility, int dexterity, int money, int experience) {
+
+    Hero *newHero = new Warrior(name,magicP,strength,agility,dexterity,money,experience);
+    Warriors.push_back(NewHero);
+}
+
+void MyTeam::Insert_Sorcerers(string name, int magicP, int strength, int agility, int dexterity, int money, int experience) {
+    Hero *NewHero = new Sorcerer(name,magicP,strength,agility,dexterity,money,experience);
+    Sorcerers.push_back(NewHero);
+}
+
+void MyTeam::Insert_Paladins(string name, int magicP, int strength, int agility, int dexterity, int money, int experience) {
+    Hero *NewHero = new Paladin(name,magicP,strength,agility,dexterity,money,experience);
+    Paladins.push_back(NewHero);
+}
+
+void MyTeam::Display_Heroes() {
+    list <Hero*> :: iterator it;
+
+    cout << " Available Warriors" << endl;
+    cout << "********************" << endl << endl;
+    cout << "Name" << "\t\t\t    " << "Magic Power" << "\t    " << "Health Power" << "\t" << "Strength" << "\t" << "Agility" << "\t      " << "Dexterity" << "\t     " << "Experience" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------------" << endl;
+    for(it = Warriors.begin(); it!=Warriors.end(); ++it)
+        cout << (*it)->get_name() << "\t\t" << (*it)->get_magicPower() << "\t\t" << (*it)->get_healthP() << "\t\t  " << (*it)->get_strength() << "\t\t  " << (*it)->get_agility() << "\t\t " << (*it)->get_dexterity() << "\t\t" << (*it)->get_experience() << endl;
+    cout << endl;
+
+    cout << " Available Sorcerers" << endl;
+    cout << "*********************" << endl << endl;
+    cout << "Name" << "\t\t\t    " << "Magic Power" << "\t    " << "Health Power" << "\t" << "Strength" << "\t" << "Agility" << "\t      " << "Dexterity" << "\t     " << "Experience" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------------" << endl;
+    for(it = Sorcerers.begin(); it!=Sorcerers.end(); ++it)
+        cout << (*it)->get_name() << "\t\t" << (*it)->get_magicPower() << "\t\t" << (*it)->get_healthP() << "\t\t  " << (*it)->get_strength() << "\t\t  " << (*it)->get_agility() << "\t\t " << (*it)->get_dexterity() << "\t\t" << (*it)->get_experience() << endl;
+    cout << endl;
+
+    cout << " Available Paladins" << endl;
+    cout << "********************" << endl << endl;
+    cout << "Name" << "\t\t\t    " << "Magic Power" << "\t    " << "Health Power" << "\t" << "Strength" << "\t" << "Agility" << "\t      " << "Dexterity" << "\t     " << "Experience" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------------" << endl;
+    for(it = Paladins.begin(); it!=Paladins.end(); ++it)
+    cout << (*it)->get_name() << "\t\t" << (*it)->get_magicPower() << "\t\t" << (*it)->get_healthP() << "\t\t  " << (*it)->get_strength() << "\t\t  " << (*it)->get_agility() << "\t\t " << (*it)->get_dexterity() << "\t\t" << (*it)->get_experience() << endl;
+    cout << endl;
+}
+
+std::string MyTeam::catch_wrongName(string name) {
+    string ch_name;
+    list <Hero*> :: iterator it;
+    int found = 0;
+    for(it = Warriors.begin(); it!=Warriors.end(); ++it) {
+        if((*it)->get_name() == name)
+            return name;
+    }
+    for(it = Sorcerers.begin(); it!=Sorcerers.end(); ++it) {
+        if((*it)->get_name() == name)
+            return name;
+    }
+    for(it = Paladins.begin(); it!=Paladins.end(); ++it) {
+        if((*it)->get_name() == name)
+            return name;
+    }
+    while(found == 0) {
+        cout << "Wrong Name for Hero. Please try again." << endl;
+        string n;
+        cin >> n;
+        for(it = Warriors.begin(); it!=Warriors.end(); ++it) {
+            if((*it)->get_name() == n) {
+                ch_name = n;
+                found = 1;
+                break;
+            }
+        }
+        for(it = Sorcerers.begin(); it!=Sorcerers.end(); ++it) {
+            if((*it)->get_name() == n) {
+                ch_name = n;
+                found = 1;
+                break;
+            }
+        }
+        for(it = Paladins.begin(); it!=Paladins.end(); ++it) {
+            if((*it)->get_name() == n) {
+                ch_name = n;
+                found = 1;
+                break;
+            }
+        }
+    }
+    return ch_name;
+}
+
+Hero* MyTeam::Search_Hero(string name) {
+    list <Hero*> :: iterator it;
+
+    for(it = Warriors.begin(); it!=Warriors.end(); ++it) {
+        if((*it)->get_name() == name)
+            return *it;
+    }
+    for(it = Sorcerers.begin(); it!=Sorcerers.end(); ++it) {
+        if((*it)->get_name() == name)
+            return *it;
+    }
+    for(it = Paladins.begin(); it!=Paladins.end(); ++it) {
+        if((*it)->get_name() == name)
+            return *it;
+    }
+    return NULL;
+}
+
+void MyTeam::add_player(std::string name) {
+    if(my_heroes.size() >= 3) {
+        cout << "Squads with more than 3 members are not allowed" << endl;
+        return;
+    }
+    else {
+        Hero* hero = Search_hero(name);
+        if(hero->get_classType() == "Warrior") {
+            Hero *NewHero = new Warrior(name,hero->get_magicPower(),hero->get_strength(),hero->get_agility(),hero->get_dexterity(),hero->get_money(),hero->get_experience());
+            my_heroes.push_back(NewHero);
+        }
+        else if(hero->get_classType() == "Sorcerer") {
+            Hero *NewHero = new Sorcerer(name,hero->get_magicPower(),hero->get_strength(),hero->get_agility(),hero->get_dexterity(),hero->get_money(),hero->get_experience());
+            my_heroes.push_back(NewHero);
+        }
+        else {
+            Hero *NewHero = new Sorcerer(name,hero->get_magicPower(),hero->get_strength(),hero->get_agility(),hero->get_dexterity(),hero->get_money(),hero->get_experience());
+            my_heroes.push_back(NewHero);
+        }
+    }
+}
+
+void MyTeam::Display_Team() {
+    list <Hero*> :: iterator it;
+    cout << endl << endl << BOLD(FGRN("Your Team Members:")) << endl;
+    cout << "Name" << "\t\t\t   " << "Magic Power" << "\t   " << "Health Power" << "\t" << "Strength" << "\t" << "Agility" << "\t   " << "Dexterity" << "\t     " << "Experience" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------------" << endl;
+    for(it = my_heroes.begin(); it!=my_heroes.end(); ++it)
+    cout << (*it)->get_name() << "\t\t" << (*it)->get_magicPower() << "\t\t" << KGRN << (*it)->get_healthP() << KWHT << "\t\t  " << (*it)->get_strength() << "\t\t  " << (*it)->get_agility() << "\t\t " << (*it)->get_dexterity() << "\t\t" << (*it)->get_experience() << endl;
+    cout << endl;
+}
+
+void MyTeam::Display_Team(int f) {
+    list <Hero*> :: iterator it;
+    cout << endl << endl << BOLD(FGRN("Your Team Members:")) << endl;
+    cout << "Name" << "\t\t\t        " << "Money" << endl;
+    cout << "--------------------------------------------" << endl;
+    for(it = my_heroes.begin(); it!=my_heroes.end(); ++it)
+        cout << (*it)->get_name() << "\t\t" << (*it)->get_money() << " $" << endl;
+    cout << endl;
+}
+
+list <Hero*> MyTeam::get_myHeroes() {
+    return my_heroes;
+}
+
+MyTeam::~MyTeam() {
+    list <Hero*> :: iterator ti;
+
+    for(ti=Warriors.begin(); ti!=Warriors.end(); ++ti)
+        delete *ti;
+    for(ti=Sorcerers.begin(); ti!=Sorcerers.end(); ++ti)
+        delete *ti;
+    for(ti=Paladins.begin(); ti!=Paladins.end(); ++ti)
+        delete *ti;
+    for(ti=my_heroes.begin(); ti!=my_heroes.end(); ++ti)
+        delete *ti;
 }
